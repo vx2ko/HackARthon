e5cce073358677dc8684a9cec28c2e3e87ec21c0
Kiyano <kiyano.oben@gmail.com>
2019-07-25 09:14:53 -0500

    Initial Commit

---
+++ HackARthonTests/HackARthonTests.swift
@@ -0,0 +1,34 @@
+//
+//  HackARthonTests.swift
+//  HackARthonTests
+//
+//  Created by Kiyano Oben on 7/25/19.
+//  Copyright © 2019 Kiyano Oben. All rights reserved.
+//
+
+import XCTest
+@testable import HackARthon
+
+class HackARthonTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
